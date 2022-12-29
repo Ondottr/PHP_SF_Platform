2a399e589a8d1814a507fa3bae7321a8b5e1aa11
Dmytro Dyvulskyi <Eklerk397@gmail.com>
2022-12-29 04:31:54 +0100

    Cosmetics

--- src/Database/Redis.php
+++ src/Database/Redis.php
@@ -14,12 +14,12 @@ final class Redis
 
     private function __construct()
     {
-        self::$rc = new Client( [], [ 'prefix' => sprintf( '%s:%s:', SERVER_NAME, $_ENV['APP_ENV'] ), ] );
+        self::$rc = new Client( options: [ 'prefix' => sprintf( '%s:%s:', SERVER_NAME, env( 'APP_ENV' ) ) ] );
         self::$rp = self::$rc->pipeline();
 
         self::$rc
             ->select(
-                match ( $_ENV['APP_ENV'] ) {
+                match ( env( 'APP_ENV' ) ) {
                     'dev'  => 2,
                     'test' => 1,
                     'prod' => 0
