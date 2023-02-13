e67a3fba32adb664521fc8429d438902ae183ef7
Dmytro Dyvulskyi <eklerk397@gmail.com>
2023-02-13 20:39:57 +0100

    Fixed js in script tags in TemplatesCache

--- src/Core/TemplatesCache.php
+++ src/Core/TemplatesCache.php
@@ -194,8 +194,6 @@ final class TemplatesCache
             //remove HTML comments
             '/<!--(.|\s)*?-->/' => '',
             //remove HTML comments
-            '/\s+/' => ' ',
-            //remove tabs before and after HTML tags
             '/\>[^\S ]+/s' => '>',
             '/[^\S ]+\</s' => '<',
             //shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
@@ -219,20 +217,46 @@ final class TemplatesCache
             //remove quotes from HTML attributes that does not contain spaces; keep quotes around URLs!
             //$1 and $4 insert first white-space character found before/after attribute
             '~([\r\n\t ])?([a-zA-Z0-9]+)="([a-zA-Z0-9_/\\-]+)"([\r\n\t ])?~s' => '$1$2=$3$4',
-            '/(\n|^)(\x20+|\t)/' => "\n",
-            '/(\n|^)\/\/(.*?)(\n|$)/' => "\n",
-            '/\n/' => ' ',
             '/<!--.*?-->/' => '',
             '/(\x20+|\t)/' => ' ', # Delete multispace (Without \n)
             '/(["\'])\s+>/' => "$1>", # strip whitespaces between quotation ("') and end tags
             '/=\s+(["\'])/' => "=$1", # strip whitespaces between = "'
             '/ {2,}/' => ' ', # Shorten multiple whitespace sequences
-            '/<!--.*?-->|\t|(?:\r?\n[ \t]*)+/s' => '',
             '/>[^\S ]+/' => '>', # strip whitespaces after tags, except space
             '/[^\S ]+</' => '<', # strip whitespaces before tags, except space
             '/(\s)+/' => '\\1', # shorten multiple whitespace sequences
         ];
         $fileContent = preg_replace( array_keys( $replace ), array_values( $replace ), $fileContent );
+
+        /**
+         * Replace all newline characters with spaces, but only if they are not within the <script> and </script> HTML tags.
+         */
+        // Split the input string $fileContent into an array of substrings using the string "script>" as the delimiter.
+        $parts = explode( "script>", $fileContent );
+        // Initialize $fileContent as an empty string.
+        $fileContent = '';
+
+        // Check if the number of elements in the $parts array is greater than 1.
+        if ( count( $parts ) > 1 ) {
+            // If there are multiple elements, iterate through each element in $parts.
+            foreach ( $parts as $key => $part ) {
+                // Check if the last two characters of the current element are equal to "</".
+                if ( substr( $part, -2 ) !== '</' )
+                    // If they are not equal, replace all newline characters in the current element with spaces.
+                    $fileContent .= str_replace( "\n", " ", $part );
+
+                else
+                    // If the last two characters are equal to "</", concatenate the unmodified current element to $fileContent.
+                    $fileContent .= $part;
+
+                // If the current key is less than the total number of elements minus 1, concatenate "script> " to $fileContent.
+                if ( $key < count( $parts ) - 1 )
+                    $fileContent .= 'script> ';
+
+            }
+        } else
+            // If there is only one element in the $parts array, replace all newline characters in $parts[0] with spaces and store the result in $fileContent.
+            $fileContent = str_replace( "\n", " ", $parts[0] );
 
         // remove optional ending tags {@link http://www.w3.org/TR/html5/syntax.html#syntax-tag-omission}
         $remove = [ '</option>', '</li>', '</dt>', '</dd>', '</tr>', '</th>', '</td>', ];
